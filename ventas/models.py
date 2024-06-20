92dddf977fa6d29cc39d208685bde1d6fa2db873
Juan2Pablo-lab <juanpablomunozdiaz@gmail.com>
2024-06-20 11:30:46 -0500

    Caracter

--- ventas/models.py
+++ ventas/models.py
@@ -39,6 +39,6 @@ class detalleCompra(models.Model):
     compra = models.ForeignKey(Compra, on_delete=models.SET_NULL, null=True)
     producto = models.ForeignKey(Producto, on_delete=models.SET_NULL, null=True)
     subtotal = models.DecimalField(decimal_places=2, max_digits=6, default=1)
-    descripcion = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=50)
     def __str__(self):
         return f'Detalle {self.id}'
